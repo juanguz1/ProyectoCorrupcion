06476c7f20686acc21880f5cce724ee65f84c7e9
Juan Guzman <g.juan.gc@gmail.com>
2017-09-20 21:41:04 -0500

    carpeta erronea de tafo

--- app/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once 'core/App.php'; //  requiere la app
-require_once 'core/Controller.php'; // requiere el controller
--- app/views/home/index.php
+++
@@ -1,5 +0,0 @@
-<?php include($_SERVER['DOCUMENT_ROOT']."/ProyectoCorrupcion/sistema/app/views/base_dashboard.php");?>
-
- <?php startblock('main') ?>
-     The main content
-  <?php endblock() ?>
